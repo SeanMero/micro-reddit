f2fd4db27a37a504ceb01bfa06652eb6693d5e30
Sean Mero <MeroFromVero@gmail.com>
2024-12-25 18:48:54 +0800

    Add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   validates :username, :password_confirmation, presence: true
   validates :username, uniqueness: true
+  has_many :posts, dependent: :destroy
 end
